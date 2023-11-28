56193fce69a38e442cd0eaeed1cd438640d57a04
expert-tester <chunon0705@gmail.com>
2023-11-28 21:01:34 +0800

    added NavBar

---
+++ groupAssignment/src/NavBar.java
@@ -0,0 +1,100 @@
+
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel; //test might delete later
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Chun On
+ */
+public class NavBar {
+    
+    //Define variables
+    private JFrame frame;
+    private JMenuBar menuBar;
+    private JMenu mainMenu;
+    private JMenu educationMenu;
+    private JMenu profileMenu;
+    private JMenu aboutUsMenu;
+    private JMenu helpMenu;
+    private JMenu logOutMenu;
+    private JMenuItem showMainMenu;
+    private JMenuItem showEducationLesson;
+    private JMenuItem showEducationQuiz;
+    private JMenuItem showProfile;
+    private JMenuItem showAboutUs;
+    private JMenuItem showHelp;
+    private JMenuItem showLogOut;
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+    
+    public void createGui() {
+        //NavBar frame
+        frame = new JFrame("Quiz Program");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //NavBar menu
+        menuBar = new JMenuBar();
+        mainMenu = new JMenu("Main Menu");
+        educationMenu = new JMenu("Education");
+        profileMenu = new JMenu("Profile");
+        aboutUsMenu = new JMenu("About Us");
+        helpMenu = new JMenu("Help");
+        logOutMenu = new JMenu("Log Out");
+        
+        //NavBar menu item
+        showMainMenu = new JMenuItem("Show Main Menu");
+        showEducationLesson = new JMenuItem("Lesson");
+        showEducationQuiz = new JMenuItem("Quiz");
+        showProfile = new JMenuItem("Show Profile");
+        showAboutUs = new JMenuItem("Show About Us");
+        showHelp = new JMenuItem("Show Help");
+        showLogOut = new JMenuItem("Log Out Account");
+        
+        //NavBar content
+        menuBar.add(mainMenu);
+        menuBar.add(educationMenu);
+        menuBar.add(profileMenu);
+        menuBar.add(aboutUsMenu);
+        menuBar.add(helpMenu);
+        menuBar.add(logOutMenu);
+        
+        //Menu Item content
+        mainMenu.add(showMainMenu);
+        educationMenu.add(showEducationLesson);
+        educationMenu.add(showEducationQuiz);
+        profileMenu.add(showProfile);
+        aboutUsMenu.add(showAboutUs);
+        helpMenu.add(showHelp);
+        logOutMenu.add(showLogOut);
+        
+        //Card layout
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        
+        //testing card layout
+        JLabel label = new JLabel(); 
+        label.setText("bro do you even code?");
+        cardPanel.add(label,"test");
+        cardLayout.show(cardPanel, "test");
+        
+        //NavBar frame show
+        frame.setJMenuBar(menuBar);
+        frame.add(cardPanel);
+        
+        //NavBar frame settings
+        frame.setSize(800, 800);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+}
