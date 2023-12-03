bb13471af7cf8c9bbeab7e3f60f125d3f324bde2
expert-tester <chunon0705@gmail.com>
2023-12-04 01:00:12 +0800

    implement file handling

---
+++ groupAssignment/src/Main/AboutUs.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import java.io.BufferedReader;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Chun On
+ */
+public class AboutUs {
+    private static final String FILE_PATH = "text/aboutUs.txt";
+
+//    public static void main(String[] args) {
+//        if (FileHandling.checkFileExist(FILE_PATH)) {
+//            showAboutUs();
+//        }
+//        buildAboutUs(1);
+//        showAboutUs();
+//    }
+
+    public static void buildAboutUs(int language) {
+        String message = "Welcome to our program!\n\n";
+        // Language can be added
+        switch (language) {
+            case 1:
+                message += "Objective:\nOur program is dedicated to solving the Target 13.3 in SDG 13.\nIts main goal is improve education, awareness-raising, and human and institutional capacity on climate change mitigation, adaptation, impact reduction, and early warning.\n\n";
+                message += "Introduction:\nClimate change is a global challenge that requires the concerted efforts of people around the world.\nOur program aims to provide resources and knowledge to individuals and society,\nand to raise people's awareness of climate change in order to actively participate in climate action.\nWe foster people's sense of responsibility to sustainable practice through educational advocacy and awareness promotion activities.\n\n";
+                FileHandling.setFileContent(message,FILE_PATH);
+        }
+    }
+
+    public static void showAboutUs(int language) {
+        String title = "About Us";
+        BufferedReader readAboutUs = FileHandling.getFileContent(FILE_PATH);
+        String message = FileHandling.showMessage(language, readAboutUs); // 1 for English
+        int option = showInformationDialog(message, title);
+
+        if (option == 0) {
+            Help.showHelp(language);
+        }
+    }
+
+    private static int showInformationDialog(String message, String title) {
+        return JOptionPane.showOptionDialog(null, message, title, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Help", "Close"}, "Close");
+    }
+
+}
