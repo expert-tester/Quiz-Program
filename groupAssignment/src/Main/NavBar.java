d6737457ec3a4fe40fd106db56f1e8229e67c575
expert-tester <chunon0705@gmail.com>
2023-12-04 00:59:12 +0800

    implement file handling

--- groupAssignment/src/Main/NavBar.java
+++ groupAssignment/src/Main/NavBar.java
@@ -1,6 +1,9 @@
 package Main;
 
-
+import static Main.AboutUs.buildAboutUs;
+import static Main.AboutUs.showAboutUs;
+import static Main.Help.buildHelp;
+import static Main.Help.showHelp;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,13 +18,12 @@ import javax.swing.JPanel;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Chun On
  */
-public class NavBar implements ActionListener{
-    
+public class NavBar implements ActionListener {
+
     //Define variables
     private JFrame frame;
     private JMenuBar menuBar;
@@ -40,12 +42,13 @@ public class NavBar implements ActionListener{
     private JMenuItem showLogOut;
     private JPanel cardPanel;
     private CardLayout cardLayout;
-    
+    public int language = 1;
+
     public void createGui() {
         //NavBar frame
         frame = new JFrame("Quiz Program");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         //NavBar menu
         menuBar = new JMenuBar();
         mainMenu = new JMenu("Main Menu");
@@ -54,7 +57,7 @@ public class NavBar implements ActionListener{
         aboutUsMenu = new JMenu("About Us");
         helpMenu = new JMenu("Help");
         logOutMenu = new JMenu("Log Out");
-        
+
         //NavBar menu item
         showMainMenu = new JMenuItem("Show Main Menu");
         showEducationLesson = new JMenuItem("Lesson");
@@ -63,7 +66,7 @@ public class NavBar implements ActionListener{
         showAboutUs = new JMenuItem("Show About Us");
         showHelp = new JMenuItem("Show Help");
         showLogOut = new JMenuItem("Log Out Account");
-        
+
         //NavBar content
         menuBar.add(mainMenu);
         menuBar.add(educationMenu);
@@ -71,7 +74,7 @@ public class NavBar implements ActionListener{
         menuBar.add(aboutUsMenu);
         menuBar.add(helpMenu);
         menuBar.add(logOutMenu);
-        
+
         //Menu Item content
         mainMenu.add(showMainMenu);
         educationMenu.add(showEducationLesson);
@@ -80,17 +83,11 @@ public class NavBar implements ActionListener{
         aboutUsMenu.add(showAboutUs);
         helpMenu.add(showHelp);
         logOutMenu.add(showLogOut);
-        
+
         //Card layout
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
-        
-        //testing card layout
-        JLabel label = new JLabel(); 
-        label.setText("test test 1 2 3...");
-        cardPanel.add(label,"test");
-        cardLayout.show(cardPanel, "test");
-        
+
         //ActionListener for NavBar
         showMainMenu.addActionListener(this);
         showEducationLesson.addActionListener(this);
@@ -99,32 +96,50 @@ public class NavBar implements ActionListener{
         showAboutUs.addActionListener(this);
         showHelp.addActionListener(this);
         showLogOut.addActionListener(this);
-        
+
         //NavBar frame show
         frame.setJMenuBar(menuBar);
         frame.add(cardPanel);
-        
+
         //NavBar frame settings
         frame.setSize(800, 800);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    
-    public void actionPerformed(ActionEvent e){
-        if (e.getSource()== showMainMenu)
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == showMainMenu) {
             System.out.println("Main Menu"); //change later
-        if (e.getSource()== showEducationLesson)
+        }
+        if (e.getSource() == showEducationLesson) {
             System.out.println("Education Lesson"); //change later
-        if (e.getSource()== showEducationQuiz)
+        }
+        if (e.getSource() == showEducationQuiz) {
             System.out.println("Educatin Quiz"); //change later
-        if (e.getSource()== showProfile)
+        }
+        if (e.getSource() == showProfile) {
             System.out.println("Profile"); //change later
-        if (e.getSource()== showAboutUs)
-            System.out.println("About Us"); //change later
-        if (e.getSource()== showHelp)
-            System.out.println("Help"); //change later
-        if (e.getSource()== showLogOut)
+        }
+        if (e.getSource() == showAboutUs) {
+            if (FileHandling.checkFileExist("text/aboutUs.txt")) {
+                showAboutUs(language);
+            } else {
+                buildAboutUs(language);
+                showAboutUs(language);
+            }
+        }
+
+        if (e.getSource() == showHelp) {
+            if (FileHandling.checkFileExist("text/help.txt")) {
+                showHelp(language);
+            } else {
+                buildHelp(language);
+                showHelp(language);
+            }
+        }
+        if (e.getSource() == showLogOut) {
             System.out.println("Log Out"); //change later
+        }
     }
-    
+
 }
